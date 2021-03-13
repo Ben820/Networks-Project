0000fb8acae1df423f4c534dd4aa9dd308820eda
Ben820 <79290187+Ben820@users.noreply.github.com>
2021-03-13 13:47:38 +0000

    Add files via upload

--- Networks 5.py
+++ Networks 5.py
@@ -11,7 +11,7 @@ CID: 01508466
 #%% WRITE
 import pickle
 
-with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\AAAAAA', 'wb') as dummy:
+with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\Pref248', 'wb') as dummy:
     pickle.dump(data, dummy, protocol=pickle.HIGHEST_PROTOCOL)
     
 #%% READ
@@ -19,8 +19,8 @@ import pickle
 # Complete3m100k
 # Incompletem4100k
 # Data1.5M;2R
-with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\Complete3m100k', 'rb') as dummy:
-    dataA = pickle.load(dummy)
+with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\Pref248', 'rb') as dummy:
+    data = pickle.load(dummy)
 
 #%%
 import numpy as np
@@ -57,7 +57,8 @@ def Edge():
             vertex_con[Exist_ver-1].append(vertices[-1])
             degree[Exist_ver-1] += 1
             
-        if x == 2:
+        #if x == 2:
+        else:
             Exist_ver = np.random.choice(vertices[:-1])#, p = prob)
     
             if a >= 1:
@@ -74,20 +75,23 @@ def A(Iterations):
     for b in range(Iterations):
         Increment()
         Edge()
-##%%
+#%%
 # Initialisation
 
-#vertices = [1,2,3,4]
-#degree = [3,3,3,3]
-#vertex_con = [[2,3,4], [1,3,4], [1,2,4], [1,2,3]]
-##Data = [vertices, degree, vertex_con]
-##%%
+vertices = [1,2,3,4]
+degree = [3,3,3,3]
+vertex_con = [[2,3,4], [1,3,4], [1,2,4], [1,2,3]]
+#Data = [vertices, degree, vertex_con]
+###%%
 Nn = 64 # The number of nodes in the initial graph
 vertices = [i for i in range(1,Nn+1)]
 degree = [Nn-1]*Nn
-#vertex_con = [[] for x in range(Nn)]
-vertex_con = [vertices[:x] + vertices[x+1:] for x in range(Nn)]
-
+degree = [2]*Nn
+#vertex_con = [[] for o in range(Nn)]
+#vertex_con = [vertices[:x] + vertices[x+1:] for x in range(Nn)]
+vertex_con = [[o-1, o+1] for o in vertices]
+vertex_con[0] = [Nn, 2]
+vertex_con[Nn-1] = [Nn-1, 1]
 
 #for a in range(Nn):
 #    Exist_ver = np.random.choice(vertices[:-1])#, p = prob)
@@ -108,8 +112,8 @@ data = {}
 # I is the number of single grain additions (i.e. total time)
 R = 2 
 t = 10000
-g = np.array([1,2,3,4])
-#g = np.array([3])
+g = np.array([2,4,8])#,16,32,64])
+#g = np.array([1,2,3,4])
 for j in range(len(g)):
     for h in range(1,R):
         m = g[j]
@@ -118,26 +122,36 @@ for j in range(len(g)):
         A(t)
         data[g[j],h] = [degree, vertex_con]
         
-        # Condition to handle hitting the end of the list h
-        if h < R-1:
-            L = g[j]
-        if h == R-1:
-            if j >= len(g)-1:
-                L = g[j]
-            else:
-                L = g[j+1]
+#        # Condition to handle hitting the end of the list h
+#        if h < R-1:
+#            L = g[j]
+#        if h == R-1:
+#            if j >= len(g)-1:
+#                L = g[j]
+#            else:
+#                L = g[j+1]
         
-#        vertices = [1,2,3,4]
-#        degree = [3,3,3,3]
-#        vertex_con = [[2,3,4], [1,3,4], [1,2,4],[1,2,3]]
+        vertices = [1,2,3,4]
+        degree = [3,3,3,3]
+        vertex_con = [[2,3,4], [1,3,4], [1,2,4],[1,2,3]]
+        
         vertices = [i for i in range(1,Nn+1)]
         degree = [Nn-1]*Nn
-        vertex_con = [vertices[:x] + vertices[x+1:] for x in range(Nn)]
+        vertex_con = [vertices[:z] + vertices[z+1:] for z in range(Nn)]
+        
+        degree = [2]*Nn
+        #vertex_con = [[] for o in range(Nn)]
+        #vertex_con = [vertices[:x] + vertices[x+1:] for x in range(Nn)]
+        vertex_con = [[o-1, o+1] for o in vertices]
+        vertex_con[0] = [Nn, 2]
+        vertex_con[Nn-1] = [Nn-1, 1]
 
         #Data = [degree, vertex_con]
 
 # 15:43 19:28 1,1 2,1 3,1 complete; 4,1 87214/100000 process complete
 # 11:11 13:39 1,1 2,1 3,1 complete; 4,1 96206/100000 process complete
+
+# 13:25 
 #%%
 from collections import Counter 
 
@@ -147,7 +161,8 @@ Count = {}
 
 nlist = []
 klist = []
-for i in [1,2,3,4]:
+#for i in [1,2,3,4]:
+for i in [2,4,8]:
     # Count contains the number of nodes with degree x; key is degree and value is 
     # the number of nodes with that degree 
     Count = Counter(data[i,1][0]) # dataA[i,1][1] is the degree of each node 
@@ -168,7 +183,7 @@ plt.figure()
 plt.plot(klist[0], nlist[0], 'x', label = "m = 1")
 plt.plot(klist[1], nlist[1], 'x', label = "m = 2")
 plt.plot(klist[2], nlist[2], 'x', label = "m = 3")
-plt.plot(klist[3], nlist[3], 'x', label = "m = 3")
+#plt.plot(klist[3], nlist[3], 'x', label = "m = 3")
 
 plt.xlabel("Degree, $k$", size = "15")
 plt.ylabel("Degree probability, $P_\infty(k)$", size = "15")
@@ -183,20 +198,25 @@ plt.show()
 Number of nodes with degree x divided by total number of nodes vs degree """
 plt.figure()
 
-scale = 1.3
+scale = 1.2
 s0 = False # whether or not to include s = 0 avalanches 
  
 # Need to first run logbin file - Credit: Max Falkenberg McGillivray
-bin_k1 = logbin(data[1,1][0],scale, s0)
-bin_k2 = logbin(data[2,1][0],scale, s0)
-bin_k3 = logbin(data[3,1][0],scale, s0)
-bin_k4 = logbin(data[4,1][0],scale, s0)
+#bin_k1 = logbin(data[1,1][0],scale, s0)
+#bin_k2 = logbin(data[2,1][0],scale, s0)
+#bin_k3 = logbin(data[3,1][0],scale, s0)
+#bin_k4 = logbin(data[4,1][0],scale, s0)
+
+bin_k1 = logbin(data[2,1][0],scale, s0)
+bin_k2 = logbin(data[4,1][0],scale, s0)
+bin_k3 = logbin(data[8,1][0],scale, s0)
+#bin_k4 = logbin(data[4,1][0],scale, s0)
 
 
 plt.plot(bin_k1[0], bin_k1[1], 'x-', label = "m = 1")
 plt.plot(bin_k2[0], bin_k2[1], 'x-', label = "m = 2")
 plt.plot(bin_k3[0], bin_k3[1], 'x-', label = "m = 3")
-plt.plot(bin_k4[0], bin_k4[1], 'x-', label = "m = 3")
+#plt.plot(bin_k4[0], bin_k4[1], 'x-', label = "m = 4")
 
 
 plt.xlabel("Degree, $k$", size = "15")
@@ -224,3 +244,12 @@ for e in sorted(Deg.keys()):
 A(1000)
 #print((sum(degree)-6)/2)
 #print(sum(edges))
+#%%
+q = 0.5
+x = np.random.choice([1,2], p = [q,1-q])
+if x == 1:
+    print('yes')
+else:
+    print("No")
+#else:
+#    print("fuck")
