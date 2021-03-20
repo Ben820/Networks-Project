5824feab53cc6d036a93eac369a43847ea078328
Ben820 <79290187+Ben820@users.noreply.github.com>
2021-03-20 20:11:22 +0000

    Add files via upload

--- Networks 5.py
+++ Networks 5.py
@@ -11,7 +11,7 @@ CID: 01508466
 #%% WRITE
 import pickle
 
-with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\AAAAAAAAAAAAAAA', 'wb') as dummy:
+with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\Pref10k;50R32ii', 'wb') as dummy:
     pickle.dump(data, dummy, protocol=pickle.HIGHEST_PROTOCOL)
     
 #%% READ
@@ -23,8 +23,8 @@ import pickle
 # P10k3R64i2;4;8;16;32i
 # P10k5R64
 # 32ii
-with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\P10k5R64', 'rb') as dummy:
-    dataB = pickle.load(dummy)
+with open(r'C:\Users\44743\Documents\Imperial Year 3\Complexity & Networks\P10k3R64i2;4;8;16;32i', 'rb') as dummy:
+    data = pickle.load(dummy)
 
 #%%
 import numpy as np
@@ -114,15 +114,15 @@ vertex_con[Nn-1] = [Nn-1, 1]
 data = {}
 # R-1 is the number of separate networks simulated  
 # I is the number of single grain additions (i.e. total time)
-R = 4 
-t = 100000
+R = 11 
+t = 50000
 g = np.array([2,4,8,16,32])#,16,32,64])
 #g = np.array([1,2,3,4])
 for j in range(len(g)):
     for h in range(1,R):
         m = g[j]
         # q is the probability the edge is joined using preferential attachment 
-        q = 0
+        q = 1
         A(t)
         data[g[j],h] = [degree, vertex_con]
         
@@ -185,12 +185,17 @@ Count = {}
 
 # The number of nodes with total degree k; degree k is the key in the dict
 
+R = 4 
+t = 100000
+g = np.array([2,4,8,16,32])#,16,32,64])
+Nn = 32
+
 nlist = []
 klist = []
 Troubleshoot = []
 
 #for i in [1,2,3,4]:
-for i in [2,4]:#,8,16,32]:
+for i in [2,4,8,16,32]:
     # Count contains the number of nodes with degree x; key is degree and value is 
     # the number of nodes with that degree 
     Count = Counter(data[i,1][0]) # dataA[i,1][1] is the degree of each node 
@@ -231,7 +236,7 @@ plt.show()
 #%%
 """ Binned degree distribution; Degree probability vs degree 
 Number of nodes with degree x divided by total number of nodes vs degree """
-plt.figure()
+#plt.figure()
 
 scale = 1.2
 s0 = False # whether or not to include s = 0 avalanches 
@@ -242,14 +247,117 @@ s0 = False # whether or not to include s = 0 avalanches
 #bin_k3 = logbin(data[3,1][0],scale, s0)
 #bin_k4 = logbin(data[4,1][0],scale, s0)
 
-bin_k2 = logbin(data[2,1][0],scale, s0)
-bin_k4 = logbin(data[4,1][0],scale, s0)
-bin_k8 = logbin(data[8,1][0],scale, s0)
+#ind = data[4,1][0].index(3)
+#del(data[4,1][0][ind])
+##%%
+logdict = {}
+##%%
+
+bin2 = []
+bin4 = []
+bin8 = []
+bin16 = []
+bin32 = [] 
+
+A = ['bin2', 'bin4', 'bin8', 'bin16', 'bin32']
+binlist = [bin2, bin4, bin8, bin16]#, bin32]
+logdict = {}
+
+for h in range(1,R):
+    bin2.append(logbin(data[2,h][0],scale))
+    bin4.append(logbin(data[4,h][0],scale))
+    bin8.append(logbin(data[8,h][0],scale))
+    bin16.append(logbin(data[16,h][0],scale))
+    #bin32.append(logbin(data[32,h][0],scale))
+
+g = np.array([2,4,8,16])#,16,32,64])
+
+for j in range(len(g)):
+    logdict[A[j]] = binlist[j]
+
+#for i in range(len(bin16[0][0])):
+#    for k in range(len(bin16)):
+#        #bin16[k][0][i]
+#        logdict[i] = [bin16[k][1][i]]
+#%%
+errlist = []
+for t in range(len(binlist)):
+    xlist = []
+    ylist = []
+    
+    for h in range(0,R-1):
+        xlist.append(binlist[t][h][0].tolist())
+        ylist.append(binlist[t][h][1].tolist())
+    
+    #np.concatenate(xlist, 0)
+    Big_xlist = np.unique(np.concatenate(xlist, 0))
+    ##%%
+    Sampl = []
+    for a in range(len(Big_xlist)):
+       # print(a)
+        sample = []
+        for v in range(len(ylist)):
+            #print(v)
+            if Big_xlist[a] in xlist[v]:
+                sample.append(ylist[v][xlist[v].index(Big_xlist[a])])
+                
+        Sampl.append(sample)
+    
+    #logerr = [Big_xlist.tolist(), Sampl]
+    ##%%
+    err_y = []
+    avg_y = []
+    for s in range(len(Sampl)):
+        err_y.append(np.std(Sampl[s])/np.size(Sampl[s]))
+        avg_y.append(np.mean(Sampl[s]))
+    errlist.append(err_y)
+
+""" add probabilities together from same geometric means across iterations, to get
+set of values per geometric mean; calculate average and st dev """
+#%%
+""" TESTING """
+x1 = [0,1,2,3]
+x2 = [1,2,3,4]
+x3 = [2,4,5]
+y1 = [10,20,30,40]
+y2 = [11,21,31,41]
+y3 = [12,22,32]
+##%%
+
+big_x = [x1, x2, x3]
+big_y = [y1, y2, y3]
+Big_x = np.unique(np.concatenate(big_x))
+##%%
+Samp = []
+for a in range(len(Big_x)):
+    sample = []
+    for v in range(len(big_y)):
+        if Big_x[a] in big_x[v]:
+            sample.append(big_y[v][big_x[v].index(a)])
+    Samp.append(sample)
+#
+##%%
+##bin2[0], bin2[1]
+#
+#S = {}
+#
+#for j in range(len(g)):
+#    for p in range(1,R):
+#        for f in range(len(bin2[p])): # bin2[p] __> binlist[j][p]
+#            S[j,p] = [[], []]
+#
+#%%
+
+bin_k2 = logbin(data[2,2][0],scale, s0)
+bin_k4 = logbin(data[4,3][0],scale, s0)
+bin_k8 = logbin(data[8,2][0],scale, s0)
 bin_k16 = logbin(data[16,1][0],scale, s0)
 bin_k32 = logbin(data[32,1][0],scale, s0)
 #bin_k64 = logbin(data[64,3][0],scale, s0)
 
+#bin16 = [logbin(data[16,1][0],scale), logbin(data[16,2][0],scale, s0), logbin(data[16,3][0],scale, s0)]
 
+Bins = [bin_k2, bin_k4, bin_k8, bin_k16, bin_k32]
 #bin_k1 = logbin(Data[2],scale, s0)
 #bin_k2 = logbin(Data[4],scale, s0)
 #bin_k3 = logbin(Data[8],scale, s0)
@@ -272,7 +380,43 @@ plt.plot(bin_k8[0], bin_k8[1], 'x-', label = "m = 8")
 plt.plot(bin_k16[0], bin_k16[1], 'x-', label = "m = 16")
 plt.plot(bin_k32[0], bin_k32[1], 'x-', label = "m = 32")
 #plt.plot(bin_k64[0], bin_k64[1], 'x-', label = "m = 64")
+##%%
 
+
+
+
+
+
+
+
+
+
+
+
+def Pref_Deg_dist(k, m):
+    A = 2*m*(m+1)
+    B = (k+2)*(k+1)*k
+    y = A/B
+    return y
+
+def Rand_Deg_dist(k, m):
+    A = m**(k-m)
+    B = (1+m)**(1+k-m)
+    y = A/B
+    return y
+
+#arO = np.arange(2, 400, 0.01)
+#p0 = np.array([20])
+#p, cov = opt.curve_fit(Deg_dist, bin_k2[0], bin_k2[1], p0)
+#plt.plot(arO, Deg_dist(arO, p[0]), zorder=10,color = 'red')
+
+func = Rand_Deg_dist
+
+for j in range(len(g)):
+    arO = np.arange(g[j], np.amax(Bins[j][0]), 0.01)
+    p0 = np.array([2])
+    #p, cov = opt.curve_fit(func, Bins[j][0], Bins[j][1], p0)
+   # plt.plot(func(arO, g[j]), zorder=10,color = 'red')
 
 
 plt.xlabel("Degree, $k$", size = "15")
